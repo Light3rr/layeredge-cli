fcf8c0e324d72663081994d1fb9ed880cf9530b3
Light3rr <locomaildrive25@gmail.com>
2025-03-22 22:48:56 +0530

    Update layeredge.sh

--- layeredge.sh
+++ layeredge.sh
@@ -119,7 +119,8 @@ configure_environment() {
     echo -e "${GREEN}Private key captured: $private_key${NC}"
 
     cat > .env << EOL
-GRPC_URL=34.31.74.109:9090
+GRPC_URL=grpc.testnet.layeredge.io:9090
+#changed grpc url from official docs
 CONTRACT_ADDR=cosmos1ufs3tlq4umljk0qfe8k5ya0x6hpavn897u2cnf9k0en9jr7qarqqt56709
 ZK_PROVER_URL=http://127.0.0.1:3001
 API_REQUEST_TIMEOUT=100
